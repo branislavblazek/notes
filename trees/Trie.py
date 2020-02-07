caafce48ab3035ce478d7c55c94ae7efc9610dc3
branislavblazek <branislav.blazek.bb@gmail.com>
2020-02-08 00:35:14 +0100

    small change

--- trees/Trie.py
+++ trees/Trie.py
@@ -1,6 +1,32 @@
 class Trie:
     def __init__(self, node=None):
         self.root = TrieNode() if node is None else node
+    
+    def insert(self, word, value):
+        node = self.root
+
+        for letter in word:
+            index = index_from_letter(letter)
+            if node.children[index] is not None:
+                node = node.children[index]
+            else:
+                new_node = TrieNode()
+                node.children[index] = new_node
+                node = new_node
+        node.value = value
+        node.accepting = True
+
+    def search(self, word):
+        node = self.root
+
+        for letter in word:
+            index = index_from_letter(letter)
+            if node.children[index] is not None:
+                node = node.children[index]
+            else:
+                return None
+        if node.accepting:
+            return node.value
 
 class TrieNode:
     def __init__(self, value=None):
@@ -11,33 +37,7 @@ class TrieNode:
 def index_from_letter(letter):
     return ord(letter) - ord('a')
 
-def insert(trie, word, value):
-    node = trie.root
-
-    for letter in word:
-        index = index_from_letter(letter)
-        if node.children[index] is not None:
-            node = node.children[index]
-        else:
-            new_node = TrieNode()
-            node.children[index] = new_node
-            node = new_node
-    node.value = value
-    node.accepting = True
-
-def search(trie, word):
-    node = trie.root
-
-    for letter in word:
-        index = index_from_letter(letter)
-        if node.children[index] is not None:
-            node = node.children[index]
-        elif node.children[index] is None:
-            return None
-    if node.accepting:
-        return node.value
-
 trie = Trie()
-insert(trie, "ahoj", "Hello")
-insert(trie, "ab", "ab")
-print(search(trie, "aho"))
+trie.insert("ahoj", "Hello")
+trie.insert("ab", "ab")
+print(trie.search("ahoj"))
